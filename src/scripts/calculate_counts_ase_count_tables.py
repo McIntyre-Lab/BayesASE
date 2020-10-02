5c558640c496619be8ecdc506bc5f47ac7ae9ebb
Oleksandr Moskalenko <om@rc.ufl.edu>
2020-10-02 14:15:43 -0400

    First pass clean up for calculate_counts_ase_count_tables.py

--- src/scripts/calculate_counts_ase_count_tables.py
+++ src/scripts/calculate_counts_ase_count_tables.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import argparse
 import os
@@ -10,7 +10,6 @@ from functools import reduce
 
 DEBUG = False
 
-##calculate priors and output one file per input comparate (prior_g1, prior_g2, prior_both in the comparate)
 
 def getOptions():
     parser = argparse.ArgumentParser(description='Return best row in blast scores file')
@@ -21,6 +20,7 @@ def getOptions():
     parser.add_argument("--debug", action='store_true', default=False, help="Print debugging output")
     args=parser.parse_args()
     return(args)
+
 
 def main():
     """Main Function"""
@@ -33,36 +33,29 @@ def main():
     global DEBUG
     if args.debug: DEBUG = True
 
-    ## Read in design file as dataframe
     df_design = pd.read_csv(args.design, sep='\t', header=0)
 
-#initialize list to store dataframes for each row in design file
     sample_list = []
 
-## Read in design file  (sample = comparate name)
-#  G1,G2,sample
-
-## Read input ASE file as df, calculate prior_g1, prior_g2, prior_both and set to separate df
-## prior_df FEATURE_ID,prior_${comparate}_g1,prior_${comparate}_g2,prior_${comparate}_both
-
-#iterate over design file
-#Sample has been changed to comparate
+    ## Read in design file  (sample = comparate name)
+    #  G1,G2,sample
+    ## Read input ASE file as df, calculate prior_g1, prior_g2, prior_both and set to separate df
+    ## prior_df FEATURE_ID,prior_${comparate}_g1,prior_${comparate}_g2,prior_${comparate}_both
+    #iterate over design file
+    #Sample has been changed to comparate
     for index, sample in df_design.iterrows():
         G1 = sample['G1']
         G2 = sample['G2']
         comparate = sample['comparate']
-        prior_file = comparate + "_prior" 
+        prior_file = comparate + "_prior"
         print(prior_file)
 
         ## prior_file = sample['prior_file']
         ## prior_file is the name of the output prior file for the comparate
 
-# read in count table     
         inFile = 'ase_counts_filtered_' + comparate
         ase_df = pd.read_csv(input_dict[inFile], index_col=None, sep='\t')
         print(ase_df)
-
-        ##calculate columns for priors calculation
 
         both_list = [name for name in ase_df.columns if '_both_total_rep' in name]
         g1_list = [name for name in ase_df.columns if '_g1_total_rep' in name]
@@ -74,8 +67,6 @@ def main():
         ase_df['g2_counts'] = ase_df.loc[:,g2_list].sum(axis=1)
         ase_df['total_counts'] = ase_df.loc[:,total_list].sum(axis=1)
 
-        ### Set qsim_both as division of both / total, qsim_g1 and qsim_g2 equal to (1 - qsim_both) / 2
-
         prior_df = pd.DataFrame()
         prior_df['FEATURE_ID'] = ase_df['FEATURE_ID']
 
@@ -83,8 +74,6 @@ def main():
         prior_df['prior_' + comparate + '_g1'] = ase_df['g1_counts'] / ase_df['total_counts']
         prior_df['prior_'+ comparate + '_g2'] = ase_df['g2_counts'] / ase_df['total_counts']
 
-
-        ## Results of this division leave NaN in place of 0, so just fill with 0s
         prior_df['prior_' + comparate + '_both'].fillna(0, inplace=True)
         prior_df['prior_' + comparate + '_g1'].fillna(0, inplace=True)
         prior_df['prior_' + comparate + '_g2'].fillna(0, inplace=True)
@@ -92,6 +81,7 @@ def main():
         outfilename= prior_file
         outfile = os.path.join(args.output, outfilename)
         prior_df.to_csv(outfile, index=False, sep = '\t')
+
 
 if __name__=='__main__':
     main()
