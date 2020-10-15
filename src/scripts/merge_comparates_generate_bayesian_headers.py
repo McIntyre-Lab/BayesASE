f9475a226812412311c0f771be8d42a433f5e6f0
Oleksandr Moskalenko <om@rc.ufl.edu>
2020-10-15 15:23:59 -0400

    Merge upstream updates to merge_comparates_generate_bayesian_headers.py

--- src/scripts/merge_comparates_generate_bayesian_headers.py
+++ src/scripts/merge_comparates_generate_bayesian_headers.py
@@ -14,7 +14,6 @@ import pandas as pd
 def getOptions():
     parser = argparse.ArgumentParser(description='Merges together filtered/summarized comparate count tables by user-specified comparison')
     parser.add_argument("-output", "--output", dest="output", action="store", required=True, help="Output directory for complete merged comparate files ready for Bayesian")
-#    parser.add_argument("-comp", "--comp", dest="comp", action='store', required=True, help="Input filtered/summarized count tables per one comparate")
     parser.add_argument('-collection_identifiers','--collection_identifiers',dest='collection_identifiers', action='store', required=True, help="ASE count table collection identifiers") 
     parser.add_argument('-collection_filenames','--collection_filenames',dest='collection_filenames', action='store', required=True, help="ASE count table collection filenames")
     parser.add_argument("-design", "--design", dest="design", action='store', required=True, help="Design file")
@@ -34,23 +33,13 @@ def main():
 
     ### Read in design file as dataframe
     #Make sure design file is read as a tsv
-#    df_design = pd.read_csv(args.design)
     df_design = pd.read_csv(args.design, sep ='\t')
 
     ### Create subset of design file of comparate specification columns (will quantify # comparates by number of columns left)
     ### Store compID to name output file
 
-    c1_g1_list = df_design['C1_G1'].tolist()
-    c1_g2_list = df_design['C1_G2'].tolist()
-    c2_g1_list = df_design['C2_G1'].tolist()
-    c2_g2_list = df_design['C2_G2'].tolist()
     c1_list = df_design['Comparate_1'].tolist()
     c2_list = df_design['Comparate_2'].tolist()
-
-    del df_design['C1_G1']
-    del df_design['C1_G2']
-    del df_design['C2_G1']
-    del df_design['C2_G2']
 
 #Changed dict to dict_b since "dict" is name already used above to read collections     
     dict_b = {}
@@ -69,10 +58,6 @@ def main():
         file_list = []
         comp_dict = {}
         comparison = comparison_list[key]
-        c1_g1= c1_g1_list[key]
-        c1_g2= c1_g2_list[key]
-        c2_g1= c2_g1_list[key]
-        c2_g2= c2_g2_list[key]
         c1= c1_list[key]
         c2= c2_list[key]
         for i, comp in enumerate(row_list):
@@ -81,17 +66,12 @@ def main():
             ### Assign filename so it can be called
             # Keep file names that Galaxy assigns so that Galaxy can recognize the collection
             # Also removed extension since last script (merge with prior does not contain extension
-#            row_list[i] = args.comp + '/bayesian_input_' + comp + '.csv'
 
             identifier = 'bayesian_input_' + comp
             row_list[i] = input_dict_comp[identifier]
 
-#Change pd.read_csv to pd.read_table to read file into dataframe
-#            file = pd.read_csv(row_list[i], index_col=None, header=0)
-
             file = pd.read_table(row_list[i], index_col=None, header=0)
             file_list.append(file)
-
 
         df_merged = reduce(lambda x, y: pd.merge(x, y, on = ['FEATURE_ID']), file_list)
 
