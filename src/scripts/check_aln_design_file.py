411b267392703e0442ed3db8071e95cf414df055
Oleksandr Moskalenko <om@rc.ufl.edu>
2020-10-15 14:42:38 -0400

    Pull upstream changes for check_aln_design_file.py

--- src/scripts/check_aln_design_file.py
+++ src/scripts/check_aln_design_file.py
@@ -11,72 +11,56 @@ import logging.config
 def getOptions():
     parser = argparse.ArgumentParser(description='Check design file.  Design file MUST have following columns: G1, G2, sampleID, fqName, fqExtension, techRep, readLength')
     parser.add_argument('-design','--design',dest='design', action='store', required=True, help='Design file containing fq file names and sample ids [Required]')
-    parser.add_argument('-id','--sampleID',dest='sampleID', action='store', required=True, help='Name of the column containing sampleIDs [Required]')
-    parser.add_argument('-fq','--fqName',dest='fqName', action='store', required=True, help='Name of the column containing fastq file names [Required]')
-    parser.add_argument('-g1','--g1',dest='g1', action='store', required=True, help='Name of the column containing G1 names [Required]')
-    parser.add_argument('-g2','--g2',dest='g2', action='store', required=True, help='Name of the column containing G2 names [Required]')
-    parser.add_argument('-rep','--rep',dest='rep', action='store', required=True, help='Name of the column containing tech rep identifiers [Required]')
-    parser.add_argument('-e','--ext',dest='ext', action='store', required=True, help='fastq extension (ex: .fq, .fastq) [Required]')
-    parser.add_argument('-r','--readLen',dest='readLen', action='store', required=True, help='Name of column containing readLength values [Required]')
+    parser.add_argument('-design_identifier','--design_identifier',dest='design_identifier', action='store', required=True, help='Design file identifier [Required]')
     parser.add_argument('-d', '--dups', dest='dups', required=False, help='File containing list of duplicate fqNames in design file')
     parser.add_argument('-l', '--logfile', dest='logfile', required=True, help='Name of log file that checks design file')
-
     args = parser.parse_args()
     return(args)
 
 
-def fastq_check(design, fqName, ext):
-    """Check that names of the fastq files in the design file are unique returns duplicate rows if
-    they exist
-    """
-    with open(args.logfile, 'w') as outfile:
+def fastq_check(design):
+    fqName = 'fqName'
+    with open(args.dups, 'w') as outfile:
         df = pd.read_csv(design, sep='\t', index_col=None)
-        print(df)
-        # Compare elements in columns to a list of unique elements
         if len(df[fqName].unique().tolist()) < len(df[fqName].tolist()):
             dups = df[df.duplicated(fqName, keep=False) == True]
             if dups is not None:
-                outfile.write('There are duplicate fqNames in your design file!\n')
-                with open(args.dups, 'w') as dupout:
-                    dups.to_csv(dupout, index=False)
+                outfile.write('Duplicate check:\tThere are duplicate fqNames in your design file!')
+                outfile.write(f'\t{'-'*33}')
+                outfile.write('Duplicated Rows:\n')
+                dups.to_csv(outfile, index=False, sep="\t")
+                return -1
         else:
-            outfile.write('No duplicate fqNames in your design file' + '\n')
+            outfile.write('Duplicate check:\tNo duplicate fqNames in your design file\t\n')
+            return 0
 
-
-def columns_check(design, G1, G2, sampleID, fqName, rep, readLen):
+def columns_check(design, design_identifier):
     columns = ['G1','G2','sampleID','fqName','fqExtension','techRep','readLength']
-    with open(args.logfile, 'a') as outfile:
+    with open(args.logfile, 'w') as outfile:
         df = pd.read_csv(design, sep='\t', index_col=None)
         headers = list(df)
-        if G1 not in df:
-            outfile.write('Error: {} column does not exist in design file\n'.format(G1))
-        if G2 not in df:
-            outfile.write('Error: {} column does not exist in design file\n'.format(G2))
-        if sampleID not in df:
-            outfile.write('Error: {} column does not exist in design file\n'.format(sampleID))
-        if fqName not in df:
-            outfile.write('Error: {} column does not exist in design file\n'.format(fqName))
-        if rep not in df:
-            outfile.write('Error: {} column does not exist in design file\n'.format(rep))
-        if readLen not in df:
-            outfile.write('Error: {} column called does not exist in design file\n'.format(readLen))
         if headers != columns:
-            outfile.write('ERROR: column headers in {} do not align with order requirements\n'.format(design))
+            outfile.write(f"""Headers check:\tERROR: column headers in file {design_identifier}
+ do not align with order requirements, please check.\n{'-'*33}
+Details:\n""")
+            for col in columns:
+                if col not in df:
+                    outfile.write(f'\tError: column header {col} does not exist in design file.')
+            return -1
         if headers == columns:
-            outfile.write('Column headers in {} align with order requirements\n'.format(design))
+            outfile.write(f'Header check:\tColumns in {design_identifier} align with requirements.')
+            return 0
 
 
 def main():
     args = getOptions()
+    col_match = columns_check(args.design, args.design_identifier)
     logging.basicConfig(filename=args.logfile,
         filemode='a',
         format='%(asctime)s [%(levelname)s] %(message)s',
         level=logging.DEBUG)
     fastq_check(args.design, args.fqName, args.ext)
-    #  check that design file contains following headers (in order):
-    #	G1, G2, sampleID, fqName, fqExtension, techRep, readLength
-    #       fqName is fastq file without extension
-    columns_check(args.design, args.g1, args.g2, args.sampleID, args.fqName, args.rep, args.readLen)
+    if_dup = fastq_check(args.design)
 
 
 if __name__=='__main__':
