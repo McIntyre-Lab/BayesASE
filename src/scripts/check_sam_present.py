bc8eb3c9aa856266656c6f8f418c57832c99b472
Oleksandr Moskalenko <om@rc.ufl.edu>
2020-11-05 18:42:02 -0500

    Refactor check_sam_present.py. It still does not do anything useful under galaxy, but now it functions as designed - checking the presence of sam files.

--- src/scripts/check_sam_present.py
+++ src/scripts/check_sam_present.py
@@ -1,65 +1,88 @@
 #!/usr/bin/env python3
-import csv, sys, argparse, os, itertools, operator, collections, logging
-import logging.config
-import pandas as pd
-import numpy as np
+"""Verify that the sam files selected as tool arguments exist on the filesystem."""
 
-# McLab Packages
-#import mclib_Python as mclib
+import argparse
+import os
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Check number of reads per FQ file into and out of sam compare')
-parser.add_argument('-fq','--fq',dest='fq', action='store', required=True, help='Name of the fq file]')
-parser.add_argument('-alnType','--alnType',dest='alnType', action='store', required=False, default="SE", choices=("SE", "PE"), help='Input SE for single end or PE for paired end alignments [Default = SE]')
-parser.add_argument('-s1', '--sam1', dest='sam1', action='store', required=True, help='sam file used in sam compare script, aligned to G1 [Required]')
-parser.add_argument('-s2', '--sam2', dest='sam2', action='store', required=True, help='sam file used in sam compare script, aligned to G2 [Required]')
-#parser.add_argument('-samPath','--samPath',dest='samPath', action='store', required=True, help='Path to sam files [Required]')
-#parser.add_argument('-G1','--G1',dest='G1', action='store', required=True, help='Name of G1 [Required]')
-#parser.add_argument('-G2','--G2',dest='G2', action='store', required=True, help='Name of G2 [Required]')
 
-# output file
-parser.add_argument('-o','--out', dest='out', action='store', required=True, help='Output file containing info on whether each fq file has 2 sam files [Required]')
-args = parser.parse_args()
- 
-### For every FQ file run, should have 2 sam files
-### Append sam files to an array
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Check number of reads per FQ file into and out of sam compare"
+    )
+    parser.add_argument(
+        "-fq",
+        "--fq",
+        dest="fq",
+        action="store",
+        required=True,
+        help="Name of the fq file]",
+    )
+    parser.add_argument(
+        "-alnType",
+        "--alnType",
+        dest="alnType",
+        action="store",
+        required=False,
+        default="SE",
+        choices=("SE", "PE"),
+        help="Input SE for single end or PE for paired end alignments [Default = SE]",
+    )
+    parser.add_argument(
+        "-s1",
+        "--sam1",
+        dest="sam1",
+        action="store",
+        required=True,
+        help="sam file used in sam compare script, aligned to G1 [Required]",
+    )
+    parser.add_argument(
+        "-s2",
+        "--sam2",
+        dest="sam2",
+        action="store",
+        required=True,
+        help="sam file used in sam compare script, aligned to G2 [Required]",
+    )
+    parser.add_argument(
+        "-o",
+        "--out",
+        dest="outfile",
+        action="store",
+        required=True,
+        help="Output file containing info on whether each fq file has 2 sam files [Required]",
+    )
+    args = parser.parse_args()
+    return args
 
-fq_file = os.path.split(args.fq)[1]
 
-sam1 = args.sam1
-sam2 = args.sam2
-print(sam1)
-#samPath= args.samPath
-#sam1=args.samPath + '/' + args.G2 + '_' + args.fq + '_upd_feature_uniq.sam'
-#sam2=args.samPath + '/' + args.G1 + '_' + args.fq + '_upd_feature_uniq.sam'
+def main():
+    args = get_args()
+    fq_p = Path(args.fq)
+    fq_file = fq_p.name
+    out_path = Path(args.outfile)
+    out_dir = out_path.parent
+    # Galaxy creates a working directory for output, so don't mind that out_dir exists
+    out_dir.mkdir(exist_ok=True)
+    error = False
+    with open(out_path, "w") as out_fh:
+        out_fh.write(f"{fq_file}\tmessage\n")
+        if args.sam1 and args.sam2:
+            p_sam1 = Path(args.sam1)
+            p_sam2 = Path(args.sam2)
+            if not p_sam1.exists():
+                out_fh.write(
+                    f"{fq_file}\tError: missing {args.sam1} SAM file. Realign to updated genomes!\n"
+                )
+                error = True
+            if not p_sam2.exists():
+                out_fh.write(
+                    f"{fq_file}\tError: missing {args.sam2} SAM file. Realign to updated genomes!\n"
+                )
+                error = True
+        if not error:
+            out_fh.write(f"{fq_file}\tSuccess: located both SAM files as expected.")
 
-sarray=[]
-sarray.append(sam1)
-sarray.append(sam2)
 
-#print(sarray)
-### FQ files, num is 1 for SE, represents fq per 2 sam files
-
-### Before Sam compare
-with open(args.out, 'w') as outfile:
-    first_row = True
-
-    if first_row:
-        outfile.write('fqName' + '\t' + 'message\n')
-        first_row = False
-
-    if args.alnType=='SE':
-        num=1
-        if len(sarray) != 2*num:
-            outfile.write(fq_file + '\t' + 'Do NOT have 2 SAM files - rerun alignments to updated genomes!/n')
-        else:
-            #Continue with sam compare
-            outfile.write(fq_file + '\t' + 'Have 2 SAM files - good!\n')
-    elif args.alnType=='PE':
-        num=2
-        if len(sarray)*2 != 2*num:
-            outfile.write(fq_file + '\t' + 'Do NOT have 2 SAM files - rerun alignments to updated genomes!\n')
-        else:
-            #Continue with sam compare
-            outfile.write('Have 2 SAM files - good!\n')
-    else:
-        outfile.write(fq_file + '\t' + 'Select whether you ran SE or PE alignments!\n')
+if __name__ == "__main__":
+    main()
