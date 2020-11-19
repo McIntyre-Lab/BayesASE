4490bc35ce401ebd598b63b0d0052e489f98e92e
Oleksandr Moskalenko <om@rc.ufl.edu>
2020-11-19 17:23:50 -0500

    Lint and finish the cleanup of merge_comparates_generate_bayesian_headers, test pass.

--- src/scripts/merge_comparates_generate_bayesian_headers.py
+++ src/scripts/merge_comparates_generate_bayesian_headers.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-import re
 import argparse
-from functools import reducet
+from functools import reduce
 import pandas as pd
 
-## merge filtered/summarized files with qsim values by user-specified comparison
+# Merge filtered/summarized files with qsim values by user-specified comparison
+
+DEBUG = False
 
 
 def getOptions():
     parser = argparse.ArgumentParser(
-        description="Merges together filtered/summarized comparate count tables by user-specified comparison"
+        description="Merges filtered/summarized comparate count tables as specified by user."
     )
     parser.add_argument(
         "-output",
@@ -44,58 +45,61 @@ def getOptions():
         required=True,
         help="Design file",
     )
+    parser.add_argument(
+        "--debug", action="store_true", default=False, help="Print debugging output"
+    )
     args = parser.parse_args()
     return args
 
 
 def main():
     args = getOptions()
+    global DEBUG
+    if args.debug:
+        DEBUG = True
 
-    # Modify to allow Galaxy to take input as a collection
-
-    pattern = re.compile(r"(?<=\').*(?=\')")
-    identifiers = [
-        pattern.search(i).group() for i in args.collection_identifiers.split(",")
-    ]
+    identifiers = [i.strip() for i in args.collection_identifiers.split(",")]
     filenames = [i.strip() for i in args.collection_filenames.split(",")]
     input_dict_comp = dict(zip(identifiers, filenames))
-
-    ### Read in design file as dataframe. Make sure design file is read as a tsv
+    if DEBUG:
+        print(f"DEBUG: input dict:\n{input_dict_comp}")
 
     df_design = pd.read_csv(args.design, sep="\t")
+    if DEBUG:
+        print(f"DEBUG: design:\n{df_design}")
 
-    ### Create subset of design file of comparate specification columns (will quantify # comparates by number of columns left)
-    ### Store compID to name output file
-
+    # Create subset of design file of comparate specification columns (will quantify # comparates
+    # by number of columns left) Store compID to name output file
     c1_list = df_design["Comparate_1"].tolist()
     c2_list = df_design["Comparate_2"].tolist()
 
     # Change name of dictionary since it conflicts with dictionary name used for collections
-    dict_a = {}
+    sample_dict = {}
     row_list = []
     comparison_list = df_design["compID"].tolist()
     del df_design["compID"]
 
-    ### Create dictionaries per design file row to store the row's comparate files
+    # Create dictionaries per design file row to store the row's comparate files
     for index, sample in df_design.iterrows():
-        dict_a[index] = list(sample)
-
-    ## If there are comparison columns (column # > 1)
-    for key in dict_a:
-        row_list = dict_a[key]
+        sample_dict[index] = list(sample)
+    if DEBUG:
+        print(f"DEBUG: sample_dict:\n{sample_dict}")
+    # If there are comparison columns (column # > 1)
+    for key in sample_dict:
+        row_list = sample_dict[key]
         file_list = []
         comp_dict = {}
         comparison = comparison_list[key]
         c1 = c1_list[key]
         c2 = c2_list[key]
         for i, comp in enumerate(row_list):
+            if DEBUG:
+                print(f"DEBUG: comparison:\n{comp}")
             comp_dict[i + 1] = comp
 
-            ### Assign filename so it can be called
+            # Assign filename so it can be called
             # Keep file names that Galaxy assigns so that Galaxy can recognize the collection
-            # Also removed extension since last script (merge with prior) does not contain extension
-            identifier = "bayesian_input_" + comp
-            row_list[i] = input_dict_comp[identifier]
+            row_list[i] = input_dict_comp[comp]
 
             # Change pd.read_csv to pd.read_table to read file into dataframe
             file = pd.read_table(row_list[i], index_col=None, header=0)
@@ -103,18 +107,16 @@ def main():
 
         df_merged = reduce(lambda x, y: pd.merge(x, y, on=["FEATURE_ID"]), file_list)
 
-        ### drop columns you don't want before merge
+        # Drop columns you don't want before merge
         df_merged = df_merged[
             df_merged.columns.drop(list(df_merged.filter(regex="comp")))
         ]
 
         df_merged.set_index("FEATURE_ID", inplace=True)
 
-        ## AMM fixing below line get_values is deprecated
-        ## merged_headers = list(df_merged.columns.get_values())
         merged_headers = list(df_merged.columns.to_numpy())
-        ### For stan model, requires headers to have general comparate input names
-        ### This reassigns comparate names to be c1, c2, c3... depending on design file specifications
+        # For stan model, requires headers to have general comparate input names
+        # This reassigns comparate names to be c1, c2, c3... depending on design file specs
         for x in comp_dict:
             for i in range(len(merged_headers)):
                 if c1 in merged_headers[i]:
@@ -125,7 +127,7 @@ def main():
         df_merged.columns = merged_headers
         df_filtered = df_merged
 
-        # Output file in TSV format, and without an extension so that next script knows what name to expect
+        # Output tsv file, without an extension so that next script knows what name to expect
         outfile = args.output + "/bayesian_input_" + comparison
         df_filtered.to_csv(outfile, sep="\t")
 
