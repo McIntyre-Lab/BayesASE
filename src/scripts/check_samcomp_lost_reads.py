455e2d730536d5b684877d802a506df06b07b0be
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-06 17:13:50 -0500

    Reformat check_samcomp_lost_reads.py

--- src/scripts/check_samcomp_lost_reads.py
+++ src/scripts/check_samcomp_lost_reads.py
@@ -15,18 +15,40 @@ csv.field_size_limit(1000000000)
 def get_args():
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description="check read numbers into and out of sam compare, must be within minimum unique reads and sum of uniq reads from both summary files"
+        description="""check read numbers into and out of sam compare, must be within minimum
+                       unique reads and sum of uniq reads from both summary files"""
     )
-    parser.add_argument('-s1','--summary1',dest='summary1', action='store', required=True, help='The sam summary file containing read counts after dropping [Required]')
-    parser.add_argument('-s2','--summary2',dest='summary2', action='store', required=True, help='The sam summary file containing read counts after dropping [Required]')
-    parser.add_argument('-ase_names','--ase_names',dest='ase_names', action='store', required=True, help='fastq filename [Required]')
+    parser.add_argument(
+        "-s1",
+        "--summary1",
+        dest="summary1",
+        action="store",
+        required=True,
+        help="The sam summary file containing read counts after dropping [Required]",
+    )
+    parser.add_argument(
+        "-s2",
+        "--summary2",
+        dest="summary2",
+        action="store",
+        required=True,
+        help="The sam summary file containing read counts after dropping [Required]",
+    )
+    parser.add_argument(
+        "-ase_names",
+        "--ase_names",
+        dest="ase_names",
+        action="store",
+        required=True,
+        help="fastq filename [Required]",
+    )
     parser.add_argument(
         "-a",
         "--ase",
         dest="ase",
         action="store",
         required=True,
-        help="The ase totals file containing read counts generated from sam compare script [Required]",
+        help="ASE totals file with read counts generated from sam compare script [Required]",
     )
     parser.add_argument(
         "-o",
@@ -54,7 +76,7 @@ def main():
     galaxy/test-data/align_and_counts_test_data/check_SAM_compare_for_lost_reads_BASE_test_data.tabular
     """
     args = get_args()
-    with open(args.summary1,'r') as bwa_table:
+    with open(args.summary1, "r") as bwa_table:
         B1 = csv.reader(bwa_table, delimiter="\t")
         # Discard the header
         next(B1)
@@ -62,7 +84,7 @@ def main():
         # total reads after dropping
         uniq_b1 = int(row[1])
 
-    with open(args.summary2, 'r') as bwa2_table:
+    with open(args.summary2, "r") as bwa2_table:
         B2 = csv.reader(bwa2_table, delimiter="\t")
         # Discard the header
         next(B2)
@@ -86,8 +108,8 @@ def main():
 
     name = os.path.basename(args.ase_names)
 
-    ## counts in ase file should be betweeen minReads and the sum of uniq mapped reads
-    ## open file to write to
+    # Counts in ase file should be betweeen minReads and the sum of uniq mapped reads
+    # Open file to write to
     with open(args.out, "w") as outfile:
         writer = csv.writer(outfile, delimiter="\t")
         writer.writerow(
